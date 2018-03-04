582dcd1ea58d3d814b134ff50adc038a6d0fb363
Ram Goli <ramsgoli@gmail.com>
2018-03-03 17:21:51 -0800

    add meal-table view

---
+++ Hacktech2018/MealTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MealTableViewCell.swift
+//  Hacktech2018
+//
+//  Created by Ram Goli on 3/3/18.
+//  Copyright © 2018 Deeptanshu. All rights reserved.
+//
+
+import UIKit
+
+class MealTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
